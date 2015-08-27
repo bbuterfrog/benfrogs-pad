42fbe1743c057783a6bc7beec8a39d06fec8bdae
Ben Butler <bbutlerfrog@yahoo.com>
2015-08-27 10:47:55 -0500

    tried to fix tabs

--- main/js/index.js
+++ main/js/index.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
-	getHTML ( 'header', 'header' );
-	if ( document.documentElement.clientWidth < 600 ) {
-	    //getTabs ('head-navbar', 'mobile-head-navbar');	
-	}
-	else {
-		getTabs ('nav-header', 'nav-header' );
-	}
+	getHeader ( );
 	getHTML ( 'footer', 'footer');
 });
+
+function getHeader ( ) {
+	$.ajax ({
+		   url: '../main/php/serverHTML.php?content=header',
+		   beforeSend: showLoadingImage (div),
+		   contentType : 'html',
+		   
+	   })
+	   .done (function ( content ) {
+		   $('#header').html(content);
+		   getTabs ('nav-header', 'content')
+	   });
+}
 
 function getHTML (div, content){
    $.ajax ({
