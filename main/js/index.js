4a3236484ae5f57308eb644eaa0adc187fc422c9
Ben Butler <bbutlerfrog@yahoo.com>
2015-08-30 19:10:39 -0500

    made table responsive

--- main/js/index.js
+++ main/js/index.js
@@ -67,10 +67,11 @@ function initDeptTable () {
 		"ajax" : { 
 			"url" : '../main/php/serverHTML.php?type=JSON&content=employee-dept',
 	        "cache": false,
-            "contentType": "application/json; charset=utf-8"
+            "contentType": "application/json; charset=utf-8",
 		},
         "aoColumns" :  [
 				         {'mData': 'dept_no', 'sType': 'string', "bVisible": true},
-				         {'mData': 'dept_name', 'sType': 'string', 'bVisible': true}]   
+				         {'mData': 'dept_name', 'sType': 'string', 'bVisible': true}],
+		"responsive" : true		         
 	});
 }
