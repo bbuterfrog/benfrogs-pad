fd00d04b342111d8670537abfe862e5b0de58f1c
Ben Butler <bbutlerfrog@yahoo.com>
2015-09-01 10:25:08 -0500

    trying to adjust columns

--- main/js/index.js
+++ main/js/index.js
@@ -112,6 +112,7 @@ function initDeptTable () {
 	        	row.child( format(row.data()) ).show();
 	        	var empTable = initEmployeeTable (deptNo);
 	            tr.addClass('shown');
+	            table.columns.adjust();
 	            table.responsive.recalc();
 	        }
 	    } );
