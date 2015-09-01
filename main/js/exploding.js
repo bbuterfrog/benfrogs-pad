3198583ecd35bb5a80dae9e205269d2a62763d94
Ben Butler <bbutlerfrog@yahoo.com>
2015-09-01 18:57:55 -0500

    added datatables to exploding tables page

--- main/js/exploding.js
+++ main/js/exploding.js
@@ -1,3 +1,10 @@
+$(document).ready(function() {
+	getHeader ( );
+	getHTML ( 'footer', 'footer');
+	initStaticDeptTable();
+	initDeptTable();
+	
+});
 function getHeader ( ) {
 	$.ajax ({
 		   url: '../main/php/serverHTML.php?type=HTML&content=header',
