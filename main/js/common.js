2a1232ae84ee32494aca364ae8dff8f0ddf333e6
Ben Butler <bbutlerfrog@yahoo.com>
2015-09-01 18:50:06 -0500

    first pass at exploding tables page w/ bootstrap

---
+++ main/js/common.js
@@ -0,0 +1,27 @@
+function getHeader ( ) {
+	$.ajax ({
+		   url: '../main/php/serverHTML.php?type=HTML&content=header',
+		   beforeSend: showLoadingImage ('header'),
+		   contentType : 'html',
+		   
+	   })
+	   .done (function ( content ) {
+		   $('#header').html(content);
+});
+}
+
+function getHTML (div, content){
+   $.ajax ({
+	   url: '../main/php/serverHTML.php?type=HTML&content=' + content,
+	   beforeSend: showLoadingImage (div),
+	   contentType : 'html',
+	   
+   })
+   .done (function ( content ) {
+	   $('#'+div).html(content);
+   }); 
+}
+
+function showLoadingImage (div) {
+	$('#'+div).html('<center><img src="../main/img/loading.gif"></img></center>');
+}
