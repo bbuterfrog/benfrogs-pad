8ed9b7d37b123ac54bfce00ac45dc41ec4249577
Ben Butler <bbutlerfrog@yahoo.com>
2015-09-02 15:24:18 -0500

    added closing brace

--- main/js/self-form.js
+++ main/js/self-form.js
@@ -9,6 +9,7 @@ function populateOptions () {
    var options = $('#department');
    $.ajax ({ 
 		"url" : '../main/php/serverHTML.php?type=JSON&content=departments',
+   })	
 		.done (function ( item ) {
         $.each(content, function() {
            options.append($("<option />").val(this.ImageFolderID).text(this.Name));
