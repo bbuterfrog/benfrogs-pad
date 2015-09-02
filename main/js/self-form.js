79af8ab40c21b4365fa5b1fbcfcd54623a8fc31c
Ben Butler <bbutlerfrog@yahoo.com>
2015-09-02 15:52:39 -0500

    added counter to data

--- main/js/self-form.js
+++ main/js/self-form.js
@@ -8,8 +8,10 @@ $(document).ready(function() {
 function populateOptions () {
 	$.getJSON("../main/php/serverHTML.php?type=JSON&content=departments", function(result) {
 		var options = $("#departments");
+		var i = 0;
 		$.each(result, function() {
-		    options.append($("<option />").val(result.data.emp_no).text(result.data.dept_name));
+		    options.append($("<option />").val(result.data[i].emp_no).text(result.data[i].dept_name));
+		    $i++;
 		});
 	});     
 }
