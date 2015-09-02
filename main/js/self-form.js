845a83e4e75621b4f490545da3f2264eda029fb6
Ben Butler <bbutlerfrog@yahoo.com>
2015-09-02 15:51:14 -0500

    trying to fix options

--- main/js/self-form.js
+++ main/js/self-form.js
@@ -8,8 +8,8 @@ $(document).ready(function() {
 function populateOptions () {
 	$.getJSON("../main/php/serverHTML.php?type=JSON&content=departments", function(result) {
 		var options = $("#departments");
-		$.each(result, function(content) {
-		    options.append($("<option />").val(content.data.emp_no).text(content.data.dept_name));
+		$.each(result, function() {
+		    options.append($("<option />").val(result.data.emp_no).text(result.data.dept_name));
 		});
 	});     
 }
