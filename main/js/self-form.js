44c1bf12ab748c9d747bd1eb1d2c2f5b517cd78d
Ben Butler <bbutlerfrog@yahoo.com>
2015-09-02 16:56:57 -0500

    fixed result reference

--- main/js/self-form.js
+++ main/js/self-form.js
@@ -9,7 +9,7 @@ function populateOptions () {
 	$.getJSON("../main/php/serverHTML.php?type=JSON&content=departments", function(result) {
 	    var options = $("#options");
 	    //iterate over the data and append a select option
-	    $.each(data, function(val){
+	    $.each(result, function(val){
 	      options.append('<option id="' + val.dept_num + '">' + val.dept_name + '</option>');
 	    })
 	  });
