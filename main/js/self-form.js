0b3e12ba1f51d268547b01b4b52896d8133e47eb
Ben Butler <bbutlerfrog@yahoo.com>
2015-09-02 15:33:10 -0500

    fixed comma

--- main/js/self-form.js
+++ main/js/self-form.js
@@ -8,11 +8,11 @@ $(document).ready(function() {
 function populateOptions () {
    var options = $('#department');
    $.ajax ({ 
-		"url" : '../main/php/serverHTML.php?type=JSON&content=departments',
+		"url" : '../main/php/serverHTML.php?type=JSON&content=departments'
    })	
-		.done (function ( content ) {
-        $.each(content, function() {
-           options.append($("<option />").val(content.dept_no).text(content.dept_name));
+		.done (function ( result ) {
+        $.each(result, function() {
+           options.append($("<option />").val(this.dept_no).text(this.dept_name));
         });
    });     
         
