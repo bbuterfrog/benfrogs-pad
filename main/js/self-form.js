bd158094dec8d96df31b5a197a256a08dadff557
Ben Butler <bbutlerfrog@yahoo.com>
2015-09-02 17:51:58 -0500

    attempt to complete search form

--- main/js/self-form.js
+++ main/js/self-form.js
@@ -1,16 +1,7 @@
 $(document).ready(function() {
 	getHeader ( );
 	getHTML ( 'footer', 'footer');
-	populateOptions();
+	$( "#firstHire" ).datepicker();
+	$( "#lastHire" ).datepicker();
 	prettyPrint();
 });
-
-function populateOptions () {
-	 $.getJSON("../main/php/serverHTML.php?type=JSON&content=departments",{id: $(this).val(), ajax: 'true'}, function(j){
-	      var options = '';
-	      for (var i = 0; i < j.length; i++) {
-	        options += '<option value="' + j[i].dept_no + '">' + j[i].dept_name + '</option>';
-	      }
-	      $("select#department").html(options);
-	    });
-}
