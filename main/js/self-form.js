7d49c88e4bbefcf6b9da3ca0edb6ed265abc375c
Ben Butler <bbutlerfrog@yahoo.com>
2015-09-02 15:39:12 -0500

    trying to fix options

--- main/js/self-form.js
+++ main/js/self-form.js
@@ -12,7 +12,7 @@ function populateOptions () {
    })	
 		.done (function ( content ) {
         $.each(content, function() {
-           options.append($("<option />").val(content['dept_no']).text(conent['dept_name']));
+           options.append($("<option />").val(this['dept_no']).text(this['dept_name'));
         });
    });     
         
