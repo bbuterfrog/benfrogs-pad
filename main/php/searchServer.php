4b0425b1ea43be44f45d14357dd1e2660ba4eb4b
Ben Butler <bbutlerfrog@yahoo.com>
2015-09-03 12:35:54 -0500

    added test print

--- main/php/searchServer.php
+++ main/php/searchServer.php
@@ -1,4 +1,6 @@
 <?php
 require_once 'search.php';
 $employeeSearch = new searchEmployees ($_POST);
-die (json_encode($employeeSearch->advancedSearch()));
+print_r($employeeSearch->advancedSearch());
+die();
+die (json_encode($$employeeSearch->advancedSearch()));
