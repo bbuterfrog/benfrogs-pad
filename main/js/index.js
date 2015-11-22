1aa7c596d02c60ccb5cc9d3eb1646234cacef806
Ben Butler <bbutler@bestride.com>
2015-11-22 13:49:00 -0500

    added divs for nav-headers, headers

--- main/js/index.js
+++ main/js/index.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	var title = "Benfrog's Pad";
 	var script = "index.js";
 	getHeader ( title, script );
+	getHTML ('nav-header', 'nav-header');
 	getHTML ( 'footer', 'footer');
 });
 
