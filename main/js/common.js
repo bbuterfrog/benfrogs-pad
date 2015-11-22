b15e2f9e668e387446c4f2c0736f6a19398e0937
Ben Butler <bbutler@bestride.com>
2015-11-22 13:25:40 -0500

    changed html function

--- main/js/common.js
+++ main/js/common.js
@@ -28,6 +28,6 @@ function showLoadingImage (div) {
 }
 
 function headerSub ( content, title, script ) {
-	$(content).('div.page-title') = title;
-	$(content).('div.page-script') = script;
+	$(content).('div.page-title').html(title);
+	$(content).('div.page-script').(script);
 }
