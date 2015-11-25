a123220ad702b555add8680f2a836914f3d18553
Ben Butler <bbutler@bestride.com>
2015-11-25 11:21:52 -0500

    fixed ajax issue again

--- main/js/test.js
+++ main/js/test.js
@@ -14,14 +14,27 @@ function getHTML (div, content){
    .done (function ( content ) {
 	   $('#'+div).html(content);
 	   if ($('#small').is(":hidden")) { 
-		    getHTML ('medium', 'medium');
+		    getDiv ('medium', 'medium');
 		}
 		if ( $('#medium').is(":hidden")) {
-		   getHTML ('small', 'small');
+		   getDiv ('small', 'small');
 		}
    }); 
 }
 
 function showLoadingImage (div) {
 	$('#'+div).html('<center><img src="../main/img/loading.gif"></img></center>');
+}
+
+function getDiv ( div, content ) {
+	
+		   $.ajax ({
+			   url: '../main/php/serverHTML.php?type=HTML&content=' + content,
+			   beforeSend: showLoadingImage (div),
+			   contentType : 'html',
+		   })
+		   .done (function ( content ) {
+			   $('#'+div).html(content);
+		   }
+});
 }
