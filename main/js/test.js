b6eef0543eda52894dc8c358efe73dd80d14fe03
Ben Butler <bbutler@bestride.com>
2015-11-25 10:45:32 -0500

    added test divs

---
+++ main/js/test.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	getHeader ( );
+	getHTML ( 'test2', 'footer');
+	getHTML ('test3', 'medium');
+	getHTML ('test4', 'medium');
+});
+
+function getHTML (div, content){
+   $.ajax ({
+	   url: '../main/php/serverHTML.php?type=HTML&content=' + content,
+	   beforeSend: showLoadingImage (div),
+	   contentType : 'html',
+	   
+   })
+   .done (function ( content ) {
+	   $('#'+div).html(content);
+   }); 
+}
+
+function showLoadingImage (div) {
+	$('#'+div).html('<center><img src="../main/img/loading.gif"></img></center>');
+}
