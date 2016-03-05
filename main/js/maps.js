3883ba49ec1e1dd3dc2d5974bb184aa1e35014be
Ben Butler <benfrog@benfrog.net>
2016-03-05 17:10:17 -0500

    fix of labels on accordians

--- main/js/maps.js
+++ main/js/maps.js
@@ -2,5 +2,8 @@ $(document).ready(function() {
 	getHeader ( );
 	getHTML ( 'footer', 'footer');
 	getHTML ('example', 'mapsHowItWorks');
+	$('#mapsExample').on('shown.bs.collapse', function () {
+		getHTML ('example', 'mapsHowItWorks');
+	});
 	prettyPrint();
 });
