b24a7a45df2825e181b3eb8e81594c71d6089601
Ben Butler <benfrog@benfrog.net>
2016-03-06 11:18:53 -0500

    added maps css, added map

--- main/js/maps.js
+++ main/js/maps.js
@@ -1,6 +1,32 @@
+//universal variable for the map
+var map;
+//universal variable for mapOptions
+var mapOptions;
+
 $(document).ready(function() {
 	getHeader ( );
 	getHTML ( 'footer', 'footer');
 	getHTML ('innerHowItWorks', 'mapsHowItWorks');
 	prettyPrint();
+	 mapOptions = {
+		        center: new google.maps.LatLng(0,0),
+		        zoom: 1
+		    };
+	 google.maps.event.addDomListener(window, 'load', initialize);
 });
+
+/**
+ * This function initalizes the Google Map
+ * @param float centerLat center latitude
+ * @param float centerLon cetner longitude
+ * @param integer zoom level
+ */
+function initalize (){ 
+  map = new google.maps.Map(document.getElementById('map'), mapOptions); 
+	//Resize Function
+	google.maps.event.addDomListener(window, "resize", function() {
+		var center = map.getCenter();
+		google.maps.event.trigger(map, "resize");
+		map.setCenter(center);
+	});
+}
