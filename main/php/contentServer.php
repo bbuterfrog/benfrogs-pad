5aaa3dc12730162840c4a4a04c11f3a3684c97a5
Ben Butler <benfrog@benfrog.net>
2016-03-12 14:54:10 -0500

    fixed scope of db variable

--- main/php/contentServer.php
+++ main/php/contentServer.php
@@ -1,13 +1,13 @@
 <?php
 require_once 'database.php';
-if (isset($_GET['database'])) {
-   $database = $_GET['database'];	
-} 
-else {
-	$database = 'employees';
-}
 class contentServer extends database {
 	function __construct($database) {
+		if (isset($_GET['database'])) {
+			$database = $_GET['database'];
+		}
+		else {
+			$database = 'employees';
+		}
 		parent::__construct($database);
 	}
 		
