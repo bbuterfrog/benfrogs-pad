3bd246cb007910eac261ed7c15c19ac2c6053cca
Ben Butler <benfrog@benfrog.net>
2016-03-12 16:50:10 -0500

    another fix of file path

--- main/php/mapsServer.php
+++ main/php/mapsServer.php
@@ -45,7 +45,7 @@ switch  ($content) {
 		$data = $mapsServer->getPoints($NELat, $NELng, $SWLat, $SWLng);
 		break;
 	case 'infoBubble' :
-		$data = file_get_contents('main/infoBubble.html');
+		$data = file_get_contents('../infoBubble.html');
 		break;
 	case 'customerBubble' :
 		if(isset($_POST['addressID'])) {
