41a1de38596faf7120505dab222d470512828c0d
Ben Butler <benfrog@benfrog.net>
2016-03-12 16:52:46 -0500

    fixed infoWindow content

--- main/js/maps.js
+++ main/js/maps.js
@@ -199,7 +199,7 @@ function openInfoBubble (marker, addressID ) {
 			   var template = Handlebars.compile(templateHTML);
 			   var infoBubbleHTML = template(windowContent);
 			   var infowindow = new google.maps.InfoWindow({
-				    content: windowContent
+				    content: infoBubbleHTML
 				  });
 			   infowindow.open(map, marker);
 		   });	
