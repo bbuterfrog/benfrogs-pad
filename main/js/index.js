56bb295bc19ec85630b56f11754648d1b03583d3
Ben Butler <benfrog@benfrog.net>
2016-04-25 11:14:48 -0400

    added custom getHTML function to index.js

--- main/js/index.js
+++ main/js/index.js
@@ -3,3 +3,14 @@ $(document).ready(function() {
 	getHTML ( 'footer', 'footer');
 });
 
+function getHTML (div, content){
+	   $.ajax ({
+		   url: '../main/php/serverHTML.php?type=HTML&content=' + content,
+		   beforeSend: showLoadingImage (div),
+		   contentType : 'html',
+		   
+	   })
+	   .done (function ( content ) {
+		   $('#'+div).html(content);
+	   }); 
+	}
