7ca054e6c28e9d65e12dcbadf03e9dacb6f7d905
Ben Butler <benfrog@benfrog.net>
2017-01-08 17:12:11 -0500

    fix of about js

--- main/js/about.js
+++ main/js/about.js
@@ -2,3 +2,15 @@ $(document).ready(function() {
 	getHeader ( );
 	getHTML ( 'footer', 'footer');
 });
+
+function getHTML (div, content){
+	   $.ajax ({
+		   url: '../main/php/serverHTML.php?type=HTML&content=' + content,
+		   beforeSend: showLoadingImage (div),
+		   contentType : 'html',
+		   
+	   })
+	   .done (function ( content ) {
+		   $('#'+div).html(content);
+	   }); 
+	}
