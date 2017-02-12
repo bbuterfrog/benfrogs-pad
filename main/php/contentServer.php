541d109c7e795b100cbadd2a4031b66a87f964e5
Ben Butler <benfrog@benfrog.net>
2017-02-12 12:29:27 -0500

    small changes to docblocks

--- main/php/contentServer.php
+++ main/php/contentServer.php
@@ -1,26 +1,29 @@
 <?php
 require_once 'database.php';
+/**
+ * Gets content from either static HTML files or the database
+ */
 class contentServer extends database {
 	
-		
-	/* This function simply gets static html from a given file via
+
+	/** Simply gets static html from a given file via
 	 * a file_get_contents (simplest case for this class)
 	 * 
-	 * @param $page (string): html filename (without .html on the end) of
+	 * @param string $page: html filename (without .html on the end) of
 	 * web page you want (no path)
-	 * @return: (html), web page contents you want
+	 * @return string web page contents you want
 	 */
 	public function getHTML ( $page ) {
         $filename = "../" . $page . ".html";
 		return file_get_contents ($filename);
 	}
 	
-	/*
-	 * This function returns JSON formatted for a DataTable by querying the database
+	/**
+	 * Returns JSON formatted for a DataTable by querying the database
 	 * (using the database class) with the provided $sql string (no bound 
 	 * parameters allowed in SQL)
-	 * @param (string) $sql SQL to query db with to get JSON
-	 * @return (string) JSON to go into DataTable
+	 * @param string $sql SQL to query db with to get JSON
+	 * @return string JSON to go into DataTable
 	 * 
 	 */
 	public function getJSON ( $sql ){
@@ -29,12 +32,12 @@ class contentServer extends database {
 		return (json_encode($result));
 	}
 	
-	/*
-	 * This function returns JSON NOT formatted for a DataTable by querying the database
+	/**
+	 * Returns JSON NOT formatted for a DataTable by querying the database
 	 * (using the database class) with the provided $sql string (no bound
 	 * parameters allowed in SQL)
-	 * @param (string) $sql SQL to query db with to get JSON
-	 * @return (string) JSON 
+	 * @param string $sql SQL to query db with to get JSON
+	 * @return string JSON 
 	 *
 	 */
 	public function getSimpleJSON ( $sql ){
