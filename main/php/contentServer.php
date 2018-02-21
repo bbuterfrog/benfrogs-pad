cb95981e76dcf4217d8170b6ce644b634cdc72bd
Ben Butler <benfrog@benfrog.net>
2018-02-21 16:29:20 -0500

    added bound parameters to employee department query

--- main/php/contentServer.php
+++ main/php/contentServer.php
@@ -4,11 +4,11 @@ require_once 'database.php';
  * Gets content from either static HTML files or the database
  */
 class contentServer extends database {
-	
+
 
 	/** Simply gets static html from a given file via
 	 * a file_get_contents (simplest case for this class)
-	 * 
+	 *
 	 * @param string $page: html filename (without .html on the end) of
 	 * web page you want (no path)
 	 * @return string web page contents you want
@@ -17,27 +17,33 @@ class contentServer extends database {
         $filename = "../" . $page . ".html";
 		return file_get_contents ($filename);
 	}
-	
+
 	/**
 	 * Returns JSON formatted for a DataTable by querying the database
-	 * (using the database class) with the provided $sql string (no bound 
-	 * parameters allowed in SQL)
+	 * (using the database class) with the provided $sql string
 	 * @param string $sql SQL to query db with to get JSON
+	 * @param array $params parameters to bind with the SQL
 	 * @return string JSON to go into DataTable
-	 * 
+	 *
+	 *
 	 */
-	public function getJSON ( $sql ){
-		$resultArray = parent::query($sql);
+	public function getJSON ( $sql,  $params = array()){
+		if (!empty($params)) {
+		    $resultArray = parent::boundQuery($sql, $params);
+		}
+		else {
+			$resultArray = parent::query($sql);
+		}
 		$result = array ( 'data' => $resultArray);
 		return (json_encode($result));
 	}
-	
+
 	/**
 	 * Returns JSON NOT formatted for a DataTable by querying the database
 	 * (using the database class) with the provided $sql string (no bound
 	 * parameters allowed in SQL)
 	 * @param string $sql SQL to query db with to get JSON
-	 * @return string JSON 
+	 * @return string JSON
 	 *
 	 */
 	public function getSimpleJSON ( $sql ){
--- main/php/serverHTML.php
+++ main/php/serverHTML.php
@@ -14,17 +14,17 @@ if ( $type == 'HTML') {
 else if ( $type == 'JSON') {
 	header('Content-Type: application/json');
 	if ( $content == 'employee-dept') {
-		$sql = "SELECT d.dept_no, d.dept_name FROM departments d INNER JOIN dept_emp de ON d.dept_no = de.dept_no 
+		$sql = "SELECT d.dept_no, d.dept_name FROM departments d INNER JOIN dept_emp de ON d.dept_no = de.dept_no
                 INNER JOIN employees e on de.emp_no = e.emp_no GROUP BY d.dept_no";
 		die($server->getJSON($sql));
 	}
 	else if ($content == 'employees') {
 		$deptNo = $_GET['dept'];
-		$sql = "SELECT e.emp_no, concat(first_name, ' ', last_name ) AS name, title, CONCAT ('$', max(salary)) AS salary FROM 
-               employees e INNER JOIN salaries s ON e.emp_no = s.emp_no INNER JOIN titles t 
+		$sql = "SELECT e.emp_no, concat(first_name, ' ', last_name ) AS name, title, CONCAT ('$', max(salary)) AS salary FROM
+               employees e INNER JOIN salaries s ON e.emp_no = s.emp_no INNER JOIN titles t
                ON e.emp_no = t.emp_no INNER JOIN dept_emp de ON e.emp_no = de.emp_no WHERE
-               de.dept_no = '$deptNo' GROUP BY s.emp_no";
-		die($server->getJSON($sql));
+               de.dept_no = :deptNo GROUP BY s.emp_no";
+		die($server->getJSON($sql, array(':deptNo' => $deptNo)));
 	}
 	else if ( $content == 'departments') {
 		$sql = "SELECT dept_no, dept_name FROM departments";
